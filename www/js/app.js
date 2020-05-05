d8c056135015df27ae249a7d3fd91a5e256e0e54
netristech <admin@netris.us>
2020-05-04 23:24:53 -0400

    update

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,8 @@ var scheduleFile;
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
+    drawSchedule();
+    //drawCurrentEvent();
 	window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
 		dir.getFile("schedule.json", {create:true}, function(file) {
             scheduleFile = file;
@@ -14,6 +16,16 @@ function onDeviceReady() {
 
 function fail(err) {
     alert("Error Code " + err.code + ": " + JSON.stringify(err));
+}
+
+function drawSchedule() {
+    var hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
+    var minutes = ['00', '15', '30', '45'];
+    for (i = 0; i < hours.length; i++) {
+        for (j = 0; j < minutes.length; j++) {
+            $('#schedule-content').append(`<div class="row"><div class="col-2">${hours[i]}:${minutes[j]}</div><div id="${hours[i]}:${minutes[j]}" class="col-10"></div></div>`);
+        }
+    }
 }
 
 function readFile(fileEntry) {
@@ -40,10 +52,11 @@ function parseFile(contents) {
         let event = {
             "start": "00:00",
             "end": "23:59",
+            "color": "240,240,240",
             "events": [
                 {
                     "name": "Nothing Planned",
-                    "color": "#dddddd"
+                    "image": null
                 }
             ]
         };
@@ -58,5 +71,6 @@ function parseFile(contents) {
 }
 
 function displayEvent(event) {
-    $("#schedule").append(JSON.stringify(event));
+    //$("#schedule").append(JSON.stringify(event));
+    $("#schedule-content").append(`<div id="${event.start}" class="event" style="background-color: rgba(${event.color},0.5);">${event.events[0].name}</div>`);
 }
